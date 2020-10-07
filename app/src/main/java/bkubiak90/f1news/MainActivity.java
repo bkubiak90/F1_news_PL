d8783d441a69563caed0444837dd9c16760d2b6a
Bartlomiej Kubiak <69577868+bkubiak90@users.noreply.github.com>
2020-10-07 22:54:54 +0200

    Project Start
    created simple main_activity view; NewsItem.java class; AsyncTask and XML parser

--- app/src/main/java/bkubiak90/f1news/MainActivity.java
+++ app/src/main/java/bkubiak90/f1news/MainActivity.java
@@ -1,14 +1,70 @@
 package bkubiak90.f1news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Xml;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+    }
+
+    private class GetNews extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            InputStream inputStream = getInputStream();
+            if (null != inputStream) {
+                try {
+                    initXMLPullParser(inputStream);
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return null;
+        }
+
+        private InputStream getInputStream() {
+            try {
+                URL url = new URL("https://pl.motorsport.com/rss/f1/news/");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                return connection.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private void initXMLPullParser(InputStream inputStream) throws XmlPullParserException {
+            Log.d(TAG, "initXMLPullParser: Initializing XML Pull Parser");
+
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(inputStream, null);
+        }
     }
 }
